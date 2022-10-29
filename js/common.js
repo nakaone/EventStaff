9c05a31b6a4414f19000e28df94d0dd76a9d7d09
nakaone <57969878+nakaone@users.noreply.github.com>
2022-10-29 09:33:26 +0900

    participant.html/js, common.jsの作成

---
+++ js/common.js
@@ -0,0 +1,108 @@
+const changeScreen = (scrId='home',titleStr='お知らせ') => {  // 表示画面の切り替え
+  console.log("changeScreen start. scrId="+scrId+', titleStr='+titleStr);
+
+  // screenクラスの画面を全部隠す
+  const scrList = document.querySelectorAll('.screen');
+  for( let i=0 ; i<scrList.length ; i++ ){
+    scrList[i].style.display = 'none';
+  }
+
+  // ローディング画面以外の場合、メニュー名を書き換え
+  if( scrId !== 'loading' ){
+    document.querySelector('header .title').innerText = titleStr;
+  }
+  // 指定IDの画面は再表示
+  document.querySelector('#'+scrId).style.display = 'flex';
+
+  // メニューを隠す
+  toggleMenu(false);
+
+  // 投稿欄に名前をセット
+  if( scrId === 'home' ){
+    document.querySelector('#home input[name="from"]').value = config.handleName;
+  }
+  console.log("changeScreen end.");
+}
+
+const doGet = (endpoint,postData,callback) => {  // GASのdoGetを呼び出し、結果を返す
+  console.log("doGet start. ",postData,callback);
+
+  // GASに渡すデータを作成
+  const v = encrypt(postData,config.passPhrase);
+  dump('v',v);
+
+  // エンドポイントを作成
+  const ep = 'https://script.google.com/macros/s/〜/exec'
+    .replace('〜',endpoint) + '?v=' + v;
+  dump('ep',ep);
+
+  // GASからの返信を受けたらcallbackを呼び出し
+  fetch(ep,{"method": "GET"})
+  .then(response => response.json())
+  .then(data => {
+    console.log("doGet end.",data);
+    callback(data);  // 成功した場合、後続処理を呼び出し
+  });
+
+}
+
+const getMessages = (arg=0) => {
+  console.log('getMessages start.');
+
+  if( arg === 0 ){  // 定期的に実行される処理
+    const cTime = new Date();
+    console.log('getMessages periodical start: '+cTime.toLocaleString('ja-JP')+'.'+cTime.getMilliseconds());
+    if( config.getMessages && config.BoardIntervalId !== null){
+      // 掲示板スプレッドシートからデータを取得し、boardAreaに書き込む
+      doGet(config.BoardAPI,{func:'getMessages',data:{}},(response) => {
+        console.log('getMessages response='+JSON.stringify(response));
+        // 時系列にメッセージを並べ替え
+        response.sort((a,b) => a.timestamp < b.timestamp);
+        console.log(response);
+        // 掲示板領域に書き込むHTMLを msg として作成
+        let msg = '';
+        let lastMesDate = '1900/01/01';
+        const t = '<p class="title">[_time] From:_from　To:_to</p><p>_message</p>';
+        for( let i=0 ; i<response.length ; i++ ){
+          const dt = new Date(response[i].timestamp);
+          if( dt.toLocaleDateString('ja-JP') !== lastMesDate ){
+            lastMesDate = dt.toLocaleDateString();
+            msg += '<p class="date">' + lastMesDate + '</p>';
+          }
+          const hms = ('0'+dt.getHours()).slice(-2)
+            + ':' + ('0'+dt.getMinutes()).slice(-2)
+            + ':' + ('0'+dt.getSeconds()).slice(-2);
+          const m = t.replace('_time',hms)
+            .replace('_from',response[i].from)
+            .replace('_to',response[i].to)
+            .replace('_message',response[i].message)
+            .replace(/\n/g,'<br>');
+          console.log('m='+m);
+          msg += m;
+        }
+        // 掲示板領域に書き込み
+        const msgEl = document.getElementById('boardArea');
+        msgEl.innerHTML = msg;
+        msgEl.scrollIntoView(false);
+        console.log('getMessages periodical end: '+msg);
+      });
+    }
+  } else {  // 実行/停止指示
+    if( arg > 0 ){  // 定期巡回開始(再開)
+      config.getMessages = true;
+      //config.BoardInterval = true;
+      console.log('config='+JSON.stringify(config));
+      // 取得間隔は最低10秒。既定値30秒
+      const interval = Number(config.BoardInterval) > 9999 ? config.BoardInterval : 30000;
+//      config.BoardIntervalId = setInterval(getMessages,10000);
+      config.BoardIntervalId = setInterval(getMessages,interval);
+      console.log('getMessages start. id='+config.BoardIntervalId);
+    } else {    // 定期巡回停止
+      clearInterval(config.BoardIntervalId);
+      config.getMessages = false;
+      //config.BoardInterval = false;
+      console.log('getMessages stop. id='+config.BoardIntervalId);
+      config.BoardIntervalId = null;
+    }
+  }
+}
---
+++ js/participant.js
@@ -0,0 +1,128 @@
+const config = {
+  // 分類A
+  FormURL: "https://docs.google.com/forms/d/e/1FAIpQLSfIJ4IFsBI5pPXsLz2jlTBczzIn8QZQL4r6QHqxZmSeDOhwUA/viewform",
+  SiteURL: "https://sites.google.com/view/shimokita-oyaji/home/archives/20221001-%E6%A0%A1%E5%BA%AD%E3%83%87%E3%82%A4%E3%82%AD%E3%83%A3%E3%83%B3%E3%83%97",
+  MapURL: "materials/map.html",
+  TableURL: "materials/timetable/WBS.html",
+  EnqueteURL: "https://docs.google.com/forms/d/16r3luYQRiLVmI9xqaD4FuaSlUqTRGvI8nAGrjGcg8lc/viewform",
+  // 分類B
+  BoardAPI: null,         // 「掲示板」のGAS Web API のID
+  passPhrase: null,       // GASとの共通鍵(Master, Board共通)
+  DateOfExpiry: null,     // config情報の有効期限
+  BoardInterval: 30000,   // 掲示板巡回のインターバル。m秒
+  // 分類C
+  //handleName: '(未定義)',  // お知らせに表示する自分の名前
+  // 分類D
+  //scanCode: false,        // スキャン実行フラグ。true時のみスキャン可
+  getMessages: false,     // 掲示板データ取得フラグ。true時のみ実行可。
+  BoardIntervalId: null,  // setIntervalのID
+  // --- メソッド
+  set: (label,value) => { // 値のセット＋localStorageへの格納
+    console.log('config.set start. label='+label+', value='+JSON.stringify(value));
+    config[label] = value;
+    let sv = {};
+    for( let x in config ){
+      if( typeof config[x] !== 'function' ){
+	      sv[x] = config[x];
+      }
+    }
+    sv = JSON.stringify(sv);
+    localStorage.setItem('config',sv)
+    console.log('config.set end. sv='+sv);
+  },
+}
+
+const initialize = () => {  // 初期設定処理
+  console.log("initialize start.");
+
+  // [01] 初期設定処理の画面を表示
+  changeScreen('initialize',"初期化処理");
+
+  // [02] 画面・イベント定義の設定
+  // 01. お知らせ画面
+  // 「投稿する」ボタンの動作を定義
+  const postButton = document.querySelector('#home .postArea input');
+  postButton.addEventListener('click',() => {
+    const post = document.querySelector('#home .postMessage');
+    if( postButton.value === '投稿する' ){
+      postButton.value = '閉じる';
+      post.style.display = 'block';
+    } else {
+      postButton.value = '投稿する';
+      post.style.display = 'none';
+    }
+  });
+  // 新規のお知らせが来たら末尾を表示
+  // https://at.sachi-web.com/blog-entry-1516.html
+  const msgArea = document.getElementById('boardArea');
+  const mo = new MutationObserver(() => {
+    console.log('mutation detected');
+    msgArea.scrollTop = msgArea.scrollHeight;
+  });
+  mo.observe(msgArea,{
+    childList: true,
+    attributes: true,
+    characterData: true,
+    subtree: true,//孫以降のノードの変化も検出
+    attributeOldValue: true,//変化前の属性データを記録する
+    characterDataOldValue: true,//変化前のテキストノードを記録する
+    attributeFilter: [],//配列で記述した属性だけを見張る
+  });
+
+  // 05. 進行予定画面
+  document.querySelector("#schedule iframe").src = config.TableURL;
+  // 06. 校内案内図
+  document.querySelector("#VenueMap iframe").src = config.MapURL;
+  // 07. サイト案内　※Googleのサイトはiframe不可
+  document.querySelector('nav .noticeSite').href = config.SiteURL;
+  // 08. アンケート
+  setQRcode('#enquete .qrcode',{text:config.SiteURL});
+
+  // [03] グローバル変数 config 設定
+  // 01. 初期設定終了時の処理を事前に定義
+  const terminate = () => {
+    getMessages(1);  // 掲示板定期更新開始
+    console.log("initialize end.",config);
+    changeScreen();// ホーム画面表示
+  }
+
+  // 02. localStorageから読み込み
+  let confStr = localStorage.getItem('config');
+  if( confStr ){
+    confObj = JSON.parse(confStr);
+    if( confObj.DateOfExpiry < new Date() ){
+      // 有効期限が切れていたら無効化＋localStorageから削除
+      localStorage.removeItem('config');
+      // ハンドルネームだけは従来の設定を引き継ぐ
+      config.set('handleName',confObj.handleName);  // 分類C
+    } else {
+      // 有効期限内ならセットして以後の処理はスキップ
+      Object.assign(config,confObj);
+      terminate();
+      return;
+    }
+  }
+
+  // 03. 分類B : シートからQRコードを読み込んで設定する変数
+  config.scanCode = true;
+  scanCode((code) => {
+    const o = JSON.parse(code); // QRコード優先分は書き換え
+    for( let x in o ){ // グローバル変数configに値を設定
+      config.set(x,o[x]);
+    }
+    alert('初期設定は正常に終了しました');
+    terminate();
+  },{
+    selector:'#initialize .scanner',  // 設置位置指定
+    RegExp:new RegExp('^{.+}$'),  // JSON文字列であること
+    alert: false,  // 読み込み時、内容をalert表示しない
+  });
+}
+
+window.addEventListener('DOMContentLoaded', function(){ // 主処理
+  // Err: "Uncaught ReferenceError: jsQR is not defined"
+  // -> DOMが構築されたときに初期化処理が行われるように設定
+  // https://pisuke-code.com/jquery-is-not-defined-solution/
+  console.log("EventStaff start.",config);
+  initialize();
+});
