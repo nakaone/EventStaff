be503d50740c3ae474865ac57a3e71ac7fdd8c4a
Nakaone Kunihiro <ena.kaon@gmail.com>
2022-12-20 16:44:51 +0900

    editParticipant prototype

--- js/Participant.js
+++ js/Participant.js
@@ -231,51 +231,85 @@ class Participant {
     this.dom.title.innerText = '参加者情報の編集';
     try {
 
-      // [01] データクレンジング
-      arg.entryNo = ('000'+arg.entryNo).slice(-4);  // 0パディング
-      arg.timestamp = getJPDateTime(arg['タイムスタンプ']);
-      /* 「取消」の文字列が入っていればtrue
-      arg['取消'] = arg['取消'].length === 0 ? "無し" : "有り";
-      ['','①','②','③'].forEach(x => {
-        if( arg[x+'状態'].length === 0 )
-          arg[x+'状態'] = arg[x+'氏名'].length === 0 ? '未登録' : '未入場';
-        if( arg[x+'参加費'].length === 0 )
-          arg[x+'参加費'] = arg[x+'氏名'].length === 0 ? '無し' : '未収';
-      });*/
+      // 01. htmlの生成
+      let iHtml = '<div class="table">';  // wrapper start
+      // 01.1 申込者識別
+      iHtml += '<div class="tr">'
+      + '  <div class="td entryNo">' + (('000'+arg.entryNo).slice(-4)) + '</div>'
+      + '  <div class="td name">'
+      + '    <ruby><rb>' + arg.name00 + '</rb><rt>' + arg.yomi00 + '</rt></ruby>'
+      + '  </div>'
+      + '  <button name="details">詳細</button>'
+      + '</div>';
+      // 01.2 詳細情報
+      iHtml += '<div name="details" class="tr">'
+      + '  <div>受付日時：' + getJPDateTime(arg['タイムスタンプ']) + '</div>'
+      + '  <div>e-mail：' + arg['メールアドレス'] + '</div>'
+      + '  <div>緊急連絡先：' + arg['緊急連絡先'] + '</div>'
+      + '  <div>引取者：' + arg['引取者氏名'] + '</div>'
+      + '  <div>備考：' + arg['備考'] + '</div>'
+      + '  <div>キャンセル：' + (arg.cancel < 0 ? "取消済" : "( — )") + '</div>'
+      + '  <div>申込フォーム：<div class="qrcode"></div></div>'
+      + '</div>';
+      // 01.3 申請者の状態・参加費
+      const maruNo = ['','①','②','③'];  // 丸数字
+      for( let i=0 ; i<4 ; i++ ){
+        const num = ('0'+i).slice(-2);
+        // 空欄の場合、既定値を設定
+        if( arg['status'+num].length === 0 ){
+          arg['status'+num] = arg['name'+num].length === 0 ? '未登録' : '未入場';
+        }
+        if( arg['fee'+num].length === 0 ){
+          arg['fee'+num] = arg['name'+num].length === 0 ? '無し' : '未収';
+        }
+        iHtml += '<div>' + maruNo[i] + '</div>';  // 番号
+        iHtml += '<div>' + arg['name'+num] + '</div>';  // 氏名
+        // 状態
+        iHtml += '<div><label>入退場</label><select name="status' + num + '">';
+        let opt = ['未入場','入場済','退場済','不参加','未登録'];
+        for( let j=0 ; j<opt.length ; j++ ){
+          iHtml += '<option value="' + opt[j] + '"'
+            + ( arg['status'+num] === opt[j] ? ' selected' : '')
+            + '>' + opt[j] + '</option>';
+        }
+        iHtml += '</select></div>';
+        // 参加費
+        iHtml += '<div><label>参加費</label><select name="fee' + num + '">';
+        opt = ['未収','既収','免除','無し'];
+        for( let j=0 ; j<opt.length ; j++ ){
+          iHtml += '<option value="' + opt[j] + '"'
+            + ( arg['fee'+num] === opt[j] ? ' selected' : '')
+            + '>' + opt[j] + '</option>';
+        }
+        iHtml += '</select></div>';
+      }
+      iHtml += '<button name="update">登録</button>'
+      iHtml += '<button name="cancel">取消</button>'
+      iHtml += '</div>';  // wrapper end
+      this.dom.main.innerHTML = iHtml;
+      // 詳細情報は当初非表示に
+      this.dom.main.querySelector('div[name="details"]').style.display = 'none';
 
-      /*
-+ '<div class="table">'
-
-// 申込者識別
-+ '  <div class="tr">'
-+ '    <div class="td entryNo">' + arg.entryNo + '</div>'
-+ '    <div class="td name">'
-+ '      <ruby><rb>' + arg.name00 + '</rb><rt>' + arg.yomi00 + '</rt></ruby>'
-+ '    </div>'
-+ '    <button name="details">詳細</button>'
-+ '  </div>'
-
-// 詳細情報
-+ '  <div class="tr">'
-+ '    <div>受付日時：' + arg['タイムスタンプ'] + '</div>'
-+ '    <div>e-mail：' + arg['メールアドレス'] + '</div>'
-+ '    <div>緊急連絡先：' + arg['緊急連絡先'] + '</div>'
-+ '    <div>引取者：' + arg['引取者氏名'] + '</div>'
-+ '    <div>備考：' + arg['備考'] + '</div>'
-+ '    <div>キャンセル：' + arg['キャンセル'] + '</div>'
-+ '    <div>申込フォーム：<div class="qrcode"></div></div>'
-+ '  </div>'
-
-// 申請者の状態・参加費
-+ '  <div></div>'
-+ '</div>'
-
-// 関数化？
-+ '    <label>入退場</label>'
-+ '    <select name="status00">'
-+ '    </select>'
-+ '  </div>'
-      */
+      // 02. イベント定義
+      // 02.1 「詳細」ボタンクリック
+      this.dom.main.querySelector('button[name="details"]').addEventListener('click',() => {
+        const btn = this.dom.main.querySelector('button[name="details"]');
+        if( btn.value === '詳細' ){
+          this.dom.main.querySelector('div[name="details"]').style.display = 'flex';
+          btn.value = '閉じる';
+        } else {
+          this.dom.main.querySelector('div[name="details"]').style.display = 'none';
+          btn.value = '詳細';
+        }
+      });
+      // 02.2 「登録」ボタンクリック
+      this.dom.main.querySelector('button[name="update"]').addEventListener('click',() => {
+        this.updateParticipant();
+      });
+      // 02.3 「取消」ボタンクリック
+      this.dom.main.querySelector('button[name="cancel"]').addEventListener('click',() => {
+        this.display();
+      });
 
       console.log('editParticipant end.');
 
@@ -320,6 +354,96 @@ class Participant {
       // 「申込フォームを表示」ボタンクリック時の遷移先を定義
       document.querySelector('#editParticipant .form input[type="button"]')
         .onclick = () => window.open(arg['編集用URL'], '_blank');
+
+  editParticipant: {tag:"div", class:"table", children:[
+    {tag:"div", class:"tr entry", children:[
+      {tag:"div", class:"td entryNo", variable:"受付番号"},
+      {tag:"div", class:"td name", children:[
+        {tag:"ruby", children:[
+          {tag:"rb", variable:"氏名"},
+          {tag:"rt", variable:"読み"},
+        ]},
+      ]},
+      {tag:"div", children:[
+        {tag:"input", type:"button", value:"詳細"},
+      ]},
+    ]},
+    {tag:"div", class:"tr detail", children:[ // 詳細情報
+      {tag:"div", text:"受付日時：\t", variable:"登録日時"},
+      {tag:"div", text:"e-mail：\t", variable:"メール"},
+      {tag:"div", text:"緊急連絡先：\t", variable:"連絡先"},
+      {tag:"div", text:"引取者：\t", variable:"引取者"},
+      {tag:"div", text:"備考：\t", variable:"備考"},
+      {tag:"div", text:"キャンセル：\t", variable:"取消"},
+      {tag:"div", class:"form", children:[
+        {tag:"div", text:"申込フォーム："},
+        {tag:"div", class:"qrcode"},
+        {tag:"input", type:"button", value:"申込フォームの表示"},
+      ]},
+    ]},
+    {tag:"div", class:"tr participant", children:[ // 申請者の状態・参加費
+      {tag:"div"},
+      {tag:"div"},
+      {tag:"div", children:[
+        {tag:"label", text:"入退場"},
+        {tag:"select", class:"status", name:"status00",
+          opt:"未入場,入場済,退場済,不参加,未登録", variable:"状態"},
+      ]},
+      {tag:"div", children:[
+        {tag:"label", text:"参加費"},
+        {tag:"select", class:"fee", name:"fee00",
+          opt:"未収,既収,免除,無し", variable:"参加費"},
+      ]},
+    ]},
+    {tag:'hr'}, // 以下参加者
+    {tag:"div", class:"tr participant", children:[
+      {tag:"div", text:"①"},
+      {tag:"div", variable:"①氏名"},
+      {tag:"div", children:[
+        {tag:"label", text:"入退場"},
+        {tag:"select", class:"status", name:"status01",
+          opt:"未入場,入場済,退場済,不参加,未登録", variable:"①状態"},
+      ]},
+      {tag:"div", children:[
+        {tag:"label", text:"参加費"},
+        {tag:"select", class:"fee", name:"fee01",
+          opt:"未収,既収,免除,無し", variable:"①参加費"},
+      ]},
+    ]},
+    {tag:"div", class:"tr participant", children:[
+      {tag:"div", text:"②"},
+      {tag:"div", variable:"②氏名"},
+      {tag:"div", children:[
+        {tag:"label", text:"入退場"},
+        {tag:"select", class:"status", name:"status02",
+          opt:"未入場,入場済,退場済,不参加,未登録", variable:"②状態"},
+      ]},
+      {tag:"div", children:[
+        {tag:"label", text:"参加費"},
+        {tag:"select", class:"fee", name:"fee02",
+          opt:"未収,既収,免除,無し", variable:"②参加費"},
+      ]},
+    ]},
+    {tag:"div", class:"tr participant", children:[
+      {tag:"div", text:"③"},
+      {tag:"div", variable:"③氏名"},
+      {tag:"div", children:[
+        {tag:"label", text:"入退場"},
+        {tag:"select", class:"status", name:"status03",
+          opt:"未入場,入場済,退場済,不参加,未登録", variable:"③状態"},
+      ]},
+      {tag:"div", children:[
+        {tag:"label", text:"参加費"},
+        {tag:"select", class:"fee", name:"fee03",
+          opt:"未収,既収,免除,無し", variable:"③参加費"},
+      ]},
+    ]},
+  ]},
+
+
+
+
+
     } */
   
   }
