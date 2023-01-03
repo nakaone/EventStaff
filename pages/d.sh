d977e5b495fa6786efb4250da67c86e5c19a092f
nakaone <57969878+nakaone@users.noreply.github.com>
2023-01-03 10:27:01 +0900

    menu.htmlを作成

--- pages/d.sh
+++ pages/d.sh
@@ -2,7 +2,12 @@ echo '\n\n\n\n\n==============================================================='
 echo `TZ='Asia/Tokyo' date`
 echo '\n'
 
-node deploy.js -h:../prototype/deploy/proto.html ../prototype/deploy/a.html ../prototype/deploy/b.js ../prototype/deploy/c.css
+# -h: index.htmlのテンプレートとなるhtmlファイル。既定値`prototype.html`
+# -c: 各ページのCSSを集めたファイル。既定値`css/style.css`
+# -j: 各ページのスクリプトを集めたファイル。既定値`js/script.js`
+# その他のパラメータ: ページごとのhtml
+
+node deploy.js -h:index.html menu.html > ../r121.html
 #node deploy.js -h:proto.html a.html b.js c.css > test.html
 #node deploy.js -h:proto.html -c:../css/style.css -j:../js/script.js information.html
 
