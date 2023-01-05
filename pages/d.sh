83fb7e99500bd56db22caad8572b8595922b3812
Nakaone Kunihiro <ena.kaon@gmail.com>
2023-01-05 12:08:25 +0900

    EventStaff.htmlの編集

--- pages/d.sh
+++ pages/d.sh
@@ -7,7 +7,8 @@ echo '\n'
 # -j: 各ページのスクリプトを集めたファイル。既定値`js/script.js`
 # その他のパラメータ: ページごとのhtml
 
-node deploy.js -h:index.html menu.html authorize.html EventStaff.html > ../index.html
+node deploy.js -h:index.html menu.html authorize.html messageBoard.html reception.html cornerOperation.html entry.html referState.html information.html enquete.html system.html > ../index.html
+#node deploy.js -h:index.html menu.html authorize.html EventStaff.html > ../index.html
 #node deploy.js -h:proto.html a.html b.js c.css > test.html
 #node deploy.js -h:proto.html -c:../css/style.css -j:../js/script.js information.html
 
